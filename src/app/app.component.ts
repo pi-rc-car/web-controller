74cda56597c55a335a1779271f04d0e582c857c6
romlm <romain103paris@gmail.com>
2020-03-11 17:53:10 +0100

    set up application for rc car

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { environment } from '../environments/environment';
+import { NgForm } from '@angular/forms/forms';
+
+
+const ACTION_URL = 'http://' + environment.host + ':' + environment.port + '/action/';
+const POWER_URL = 'http://' + environment.host + ':' + environment.port + '/power/';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rc-controller';
+
+  power = 100;
+
+  constructor(private client: HttpClient) {}
+
+
+  turn_right() {
+    this.client.get(ACTION_URL, {params: new HttpParams().append('action', 'turn_right')}).subscribe((msg) => {
+      console.log('right');
+    });
+  }
+
+  turn_left() {
+    this.client.get(ACTION_URL, {params: new HttpParams().append('action', 'turn_left')}).subscribe((msg) => {
+      console.log('left');
+    });
+  }
+
+  roll_front() {
+    this.client.get(ACTION_URL, {params: new HttpParams().append('action', 'roll_front')}).subscribe((msg) => {
+      console.log('front');
+    });
+  }
+
+  roll_back() {
+    this.client.get(ACTION_URL, {params: new HttpParams().append('action', 'roll_back')}).subscribe((msg) => {
+      console.log('back');
+    });
+  }
+
+  stop() {
+    this.client.get(ACTION_URL, {params: new HttpParams().append('action', 'stop')}).subscribe((msg) => {
+      console.log('stop');
+    });
+  }
+
+  set_power(power: number) {
+    this.client.get(POWER_URL, {params: new HttpParams().append('power', power ? power.toString() : '0')}).subscribe((msg) => {
+      console.log('power: ' + power);
+    });
+  }
+
+  onsubmit(form: NgForm) {
+    const power = form.value.power;
+    this.set_power(power as number);
+  }
 }
